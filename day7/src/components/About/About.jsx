c78609dd1660bea114fec933ee61068ef6929a9c
Devendradhote001 <ddhote780@gmail.com>
2026-03-12 22:07:59 +0530

    day 7 props and drill

---
+++ day7/src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import "./About.css";
+
+const About = ({ fn }) => {
+  return (
+    <>
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ day7/src/components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+let Contact = () => {
+  return (
+    <div className="container">
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Contact;
